5797e7c95e751a3586275eca269580270d08d933
Priyam Tripathi <149374259+priyamtripathiii03@users.noreply.github.com>
2024-04-03 17:16:59 +0530

    Add files via upload

---
+++ Lecture 1/feet.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int feet,inches;
+	cout<<"Enter the feet : ";
+	cin>>feet;
+	cout<<"Enter the inches : ";
+	cin>>inches;
+	
+	cout<<"feet : "<< feet << endl;
+	cout<<"inches : "<< inches/10 << endl;
+	
+}
+
---
+++ Lecture 1/leap year.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+using namespace std;
+main()
+{
+	int start;
+	int end;
+	cout<<"Enter the starting no : ";
+	cin>>start;
+	cout<<"Enter the ending no : ";
+	cin>>end;
+	
+	int n=(end-start)/4+1;
+	cout<<"expected years: "<<n<<endl;
+	int leap[n];
+	int i,y=0;
+	for(i=start;i<=end;i++)
+	{
+		if(i%4==0)
+		{
+			leap[y]=i;
+			y++;
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		cout<< leap[i] << endl;
+	}
+}
+
